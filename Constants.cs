1d4d253dd4499594a0e0793107b303bdca8598b6
Jeremy Bohlin <jbohlin@Calamos.com>
2022-04-07 13:36:22 -0500

    Add project files.

---
+++ Constants.cs
@@ -0,0 +1,17 @@
+﻿namespace TinyMceBackwardsRadzenDatagridRepro
+{
+    public static class Constants
+    {
+        public static Dictionary<string, object> TinyMceConfig = new Dictionary<string, object>
+        {
+            {"menubar", false},
+            {"branding", false},
+            {"plugins", "lists advlist code preview"},
+            {"height", 400},
+            {"forced_root_block", "div"},
+            {"force_br_newlines", true},
+            //{"toolbar", "undo redo | bold italic underline | forecolor | bullist numlist | outdent indent | code | removeformat formatselect fontselect fontsizeselect styleselect"}
+            {"toolbar", "undo redo | removeformat bold italic underline | forecolor backcolor | bullist numlist | outdent indent | preview | code"}
+        };
+    }
+}
---
+++ Pages/IndexBase.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+
+namespace TinyMceBackwardsRadzenDatagridRepro.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        public RadzenDataGrid<Employee> EmployeeGrid;
+        public List<Employee> EmployeeList;
+        
+        protected override async Task OnInitializedAsync()
+        {
+            EmployeeList = GetEmployeeData();
+            await base.OnInitializedAsync();
+        }
+
+        public void InsertRow()
+        {
+            var employee = new Employee()
+            {
+                Id = EmployeeList.Count + 1,
+                FirstName = "A",
+                LastName = "B"
+            };
+
+            EmployeeGrid.InsertRow(employee);
+        }
+
+        public void OnCreateRow(Employee employee)
+        {
+            EmployeeList.Add(employee);
+
+            var orderedList = EmployeeList.OrderBy(s => s.Id).ToList();
+            EmployeeList = orderedList;
+        }
+
+        public void OnUpdateRow(Employee employee)
+        {
+            
+        }
+
+        public void EditRow(Employee employee)
+        {
+            EmployeeGrid.EditRow(employee);
+        }
+
+        public void SaveRow(Employee employee)
+        {
+            EmployeeGrid.UpdateRow(employee);
+
+            var orderedList = EmployeeList.OrderBy(s => s.Id).ToList();
+
+            EmployeeList = orderedList;
+        }
+
+        public void CancelEdit(Employee employee)
+        {
+            EmployeeGrid.CancelEditRow(employee);
+        }
+
+        protected List<Employee> GetEmployeeData()
+        {
+            var employeeData = new List<Employee>
+            {
+                new Employee()
+                {
+                    Id = 1,
+                    FirstName = "Joe",
+                    LastName = "Smith",
+                    Notes = "Notes1"
+                },
+
+                new Employee()
+                {
+                    Id = 2,
+                    FirstName = "Tom",
+                    LastName = "Thumb",
+                    Notes = "Notes2"
+                }
+            };
+
+            return employeeData;
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+        public string Notes { get; set; }
+    }
+}
